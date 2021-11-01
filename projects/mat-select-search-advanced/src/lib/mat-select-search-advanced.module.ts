98e44fed306a8bb8426dfce97792bd355a8d037e
zellkon <zellkon@live.com>
2021-11-01 21:49:37 +0700

    push new module

--- projects/mat-select-search-advanced/src/lib/mat-select-search-advanced.module.ts
+++ projects/mat-select-search-advanced/src/lib/mat-select-search-advanced.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatSelectSearchAdvancedComponent } from './mat-select-search-advanced.component';
-
+import { CommonModule } from '@angular/common';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -8,6 +12,12 @@ import { MatSelectSearchAdvancedComponent } from './mat-select-search-advanced.c
     MatSelectSearchAdvancedComponent
   ],
   imports: [
+    CommonModule,
+    NgxMatSelectSearchModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     MatSelectSearchAdvancedComponent
